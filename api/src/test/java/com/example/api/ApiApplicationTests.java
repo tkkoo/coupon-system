f020d2df9e6bb2c6382610d6a23abec83f2d66d6
taekyun95 <jinhoa52@kakao.com>
2023-07-27 12:21:34 +0900

    ":sparkles: 새로운 프로젝트 추가 - API 프로젝트 설정"

--- api/src/test/java/com/example/api/ApiApplicationTests.java
+++ api/src/test/java/com/example/api/ApiApplicationTests.java
@@ -1,13 +1,13 @@
-package com.example.couponsystem;
+package com.example.api;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class CouponSystemApplicationTests {
+class ApiApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+	@Test
+	void contextLoads() {
+	}
 
 }
