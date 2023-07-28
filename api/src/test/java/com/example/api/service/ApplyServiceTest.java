e2f9d48b6aad683325c25226a434119fd1baf219
taekyun95 <jinhoa52@kakao.com>
2023-07-28 12:55:08 +0900

    이슈: 쿠폰이 100개 보다 많이 발급

--- api/src/test/java/com/example/api/service/ApplyServiceTest.java
+++ api/src/test/java/com/example/api/service/ApplyServiceTest.java
@@ -3,6 +3,9 @@ package com.example.api.service;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import com.example.api.repository.CouponRepository;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -20,5 +23,27 @@ class ApplyServiceTest {
 
     long count = couponRepository.count();
     assertThat(count).isEqualTo(1L);
+  }
+
+  @Test
+  public void 여러번응모() throws InterruptedException {
+    int theadCount = 1000;
+    ExecutorService executorService = Executors.newFixedThreadPool(32);
+    CountDownLatch latch = new CountDownLatch(theadCount);
+
+    for (int i = 0; i < theadCount; i++) {
+      long userId = i;
+      executorService.submit(
+          () -> {
+            try {
+              applyService.apply(userId);
+            } finally {
+              latch.countDown();
+            }
+          });
+    }
+    latch.await();
+    long count = couponRepository.count();
+    assertThat(count).isEqualTo(100L);
   }
 }
