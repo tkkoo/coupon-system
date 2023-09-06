d1ccf9e90d30e47c1900d8edf685a1ca88d606e3
taekyun95 <jinhoa52@kakao.com>
2023-09-06 18:41:30 +0900

    producer 추가

---
+++ api/src/main/java/com/example/api/config/KafkaProducerConfig.java
@@ -0,0 +1,35 @@
+package com.example.api.config;
+
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.LongSerializer;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
+import static org.apache.kafka.clients.producer.ProducerConfig.*;
+
+@Configuration
+public class KafkaProducerConfig {
+
+  @Bean
+  public ProducerFactory<String, Long> producerFactory() {
+    Map<String, Object> config = new HashMap<>();
+    config.put(BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+    config.put(KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+    config.put(VALUE_SERIALIZER_CLASS_CONFIG, LongSerializer.class);
+    return new DefaultKafkaProducerFactory<>(config);
+  }
+
+  @Bean
+  public KafkaTemplate<String, Long> kafkaTemplate(){
+    return new KafkaTemplate<>(producerFactory());
+  }
+
+}
--- api/src/main/java/com/example/api/service/ApplyService.java
+++ api/src/main/java/com/example/api/service/ApplyService.java
@@ -1,26 +1,28 @@
 package com.example.api.service;
 
-import com.example.api.domain.Coupon;
+import com.example.api.producer.CouponCreateProducer;
 import com.example.api.repository.CouponCountRepository;
 import com.example.api.repository.CouponRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ApplyService {
-    private final CouponRepository couponRepository;
-    private final CouponCountRepository couponCountRepository;
+  private final CouponCountRepository couponCountRepository;
+  private final CouponCreateProducer couponCreateProducer;
 
-    public ApplyService(CouponRepository couponRepository, CouponCountRepository couponCountRepository) {
-        this.couponRepository = couponRepository;
-        this.couponCountRepository = couponCountRepository;
+  public ApplyService(
+      CouponCountRepository couponCountRepository,
+      CouponCreateProducer couponCreateProducer) {
+    this.couponCountRepository = couponCountRepository;
+    this.couponCreateProducer = couponCreateProducer;
+  }
+
+  public void apply(Long userId) {
+    long count = couponCountRepository.increment();
+    if (count > 100) {
+      return;
     }
 
-    public void apply(Long userId){
-        long count = couponCountRepository.increment();
-        if (count > 100){
-            return;
-        }
-
-        couponRepository.save(new Coupon(userId));
-    }
+    couponCreateProducer.create(userId);
+  }
 }
